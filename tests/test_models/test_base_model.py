844f92d6a22884a62ed2e3f2ebc8aa22e26f35ee
Mubarak <adesinamubarak123@gmail.com>
2023-03-07 18:42:01 +0100

    base_model test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+""" Module to test BaseModel class attributes and functions """
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class test_BaseModel(unittest.TestCase):
+    """ Test class for all BaseModel test functions """
+
+    def setUp(self):
+        """ to initialize instance of object """
+        self.b1 = BaseModel()
+
+    def test_attr_types(self):
+        """ function to test the datatypes of all attributes """
+        self.assertIsInstance(self.b1, BaseModel)
+        self.assertIsInstance(self.b1.id, str)
+        self.assertIsInstance(self.b1.created_at, datetime)
+        self.assertIsInstance(self.b1.updated_at, datetime)
+
+
+if __name__ == "__main__":
+    unittest.main()
